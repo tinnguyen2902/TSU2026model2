14ecbad791790f1b060c592c03217ea0f0825012
tinnguyen <tinnguyen2922000@gmail.com>
2026-03-05 23:58:49 +0900

    Fix nested git folders

---
+++ Documents/Model3/Bai_tap/Session2/Lession1/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Scanner sc = new Scanner(System.in);
+    // nhập vào chiều dài chiều rộng
+    System.out.print("Nhập chiều dài của hình chữ nhật:");
+    double width = sc.nextDouble();
+    System.out.print("Nhập chiều dài hình chữ nhật:");
+    double height = sc.nextDouble();
+    // nhập công thức
+    double area = width * height;
+    double circumference = 2 * (width+height);
+    // in ra kết quả
+    System.out.printf("Diện tích hình chữ nhật là:%.2f",area);
+    System.out.printf("Chu vi hình chữ nhật là:%.2f",circumference);
+    sc.close();
+}
